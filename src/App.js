9d8dd0c3f2b5368e16d8cc31aaa106750da65780
Lukasowy <lukasowy@gmail.com>
2018-10-10 21:38:31 +0200

    BrowserRouter Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/Navbar'
+import {BrowserRouter, Route} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 class App extends Component {
  
   render() {
     return (
-      <div className="todo-app container"> 
-      </div>
+      <BrowserRouter>
+        <div className="App">
+            <Navbar/>
+            <Route exact path='/' component={Home}/> 
+            <Route path='/about' component={About}/> 
+            <Route path='/contact' component={Contact}/> 
+        </div>
+      </BrowserRouter>
     );
   }
 }
