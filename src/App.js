cdaa3a1bfef6a073e6c9dc50962e1f8d71027da0
Lukasz <lukasowy@gmail.com>
2018-10-11 14:22:53 +0200

    Axios

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import './App.css';
 import Navbar from './components/Navbar'
-import {BrowserRouter, Route} from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import { Route, BrowserRouter, Switch } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Post from './components/Post'
 
 class App extends Component {
- 
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-            <Navbar/>
-            <Route exact path='/' component={Home}/> 
-            <Route path='/about' component={About}/> 
-            <Route path='/contact' component={Contact}/> 
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            <Route path='/:post_id' component={Post} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
