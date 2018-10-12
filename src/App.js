a4091965bbdab8a34110d1063a3c27f5b0c6eb83
Lukasz <lukasowy@gmail.com>
2018-10-12 14:08:59 +0200

    Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Dashboard from './components/dashboard/Dashboard'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">    
-        siema
-      </div>
+      <BrowserRouter>
+        <div className="App">    
+          <Navbar/>
+          <Switch>
+            <Route path='/' component={Dashboard}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
