491495adf157ae353df7a3a4a97079f53a5ff1e3
Lukasz <lukasowy@gmail.com>
2018-10-23 07:18:42 +0200

    lukasplan

---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,14 @@
+const initState = {
+    projects:[
+        {id: 1, title: 'jajdjajd', content: 'dasskjdnaskldj'},
+        {id: 2, title: 'jajdjajd', content: 'dasskjdnaskldj'},
+        {id: 3, title: 'jajdjajd', content: 'dasskjdnaskldj'}
+    ]
+}
+
+const projectReducer = (state = initState, action) => {
+    return state;
+}
+
+
+export default projectReducer
