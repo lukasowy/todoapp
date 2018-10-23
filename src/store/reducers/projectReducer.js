1c8bbd4376ed14e043968274d239eb914be2af27
Lukasz <lukasowy@gmail.com>
2018-10-23 14:41:45 +0200

    lukasplan

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,14 +1,17 @@
 const initState = {
     projects:[
-        {id: 1, title: 'jajdjajd', content: 'dasskjdnaskldj'},
-        {id: 2, title: 'jajdjajd', content: 'dasskjdnaskldj'},
-        {id: 3, title: 'jajdjajd', content: 'dasskjdnaskldj'}
+        {id: 1, title: 'help me find peach', content: 'dassk jdna skldj'},
+        {id: 2, title: 'collect all the stars', content: 'da sskjd naskldj'},
+        {id: 3, title: 'egg hunt with sumohi', content: 'das skjdn askldj'}
     ]
 }
 
 const projectReducer = (state = initState, action) => {
+    switch(action.type){
+        case 'CREATE_PROJECT':
+            console.log('created project', action.project)
+    }
     return state;
 }
-
 
 export default projectReducer
